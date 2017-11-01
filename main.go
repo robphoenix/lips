0d8c9d76ec479bb4af5ce9d2112f0192df94eaf2
Rob Phoenix <rob.phoenix@bet365.com>
2017-11-01 18:39:49 +0000

    First pass

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	addr := os.Args[1]
+	ip, ipnet, err := net.ParseCIDR(addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i := ip.Mask(ipnet.Mask); ipnet.Contains(i); inc(i) {
+		fmt.Println(i)
+	}
+}
+
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
